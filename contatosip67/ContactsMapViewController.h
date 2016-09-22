3a8125ea36fe0692f668df47853be1b655986dfe
ios6246 <ios6246@Mac-mini-de-caelum-7.local>
2016-09-22 15:35:03 -0300

    Add ContactsMapViewController

---
+++ contatosip67/ContactsMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsMapViewController.h
+//  contatosip67
+//
+//  Created by ios6246 on 9/22/16.
+//  Copyright © 2016 ios6246. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContactsMapViewController : NSObject
+
+@end
---
+++ contatosip67/ContactsMapViewController.m
@@ -0,0 +1,13 @@
+//
+//  ContactsMapViewController.m
+//  contatosip67
+//
+//  Created by ios6246 on 9/22/16.
+//  Copyright © 2016 ios6246. All rights reserved.
+//
+
+#import "ContactsMapViewController.h"
+
+@implementation ContactsMapViewController
+
+@end
