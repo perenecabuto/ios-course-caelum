8fce8ffb2c2cc213712201650daf4148f2eece61
ios6246 <ios6246@Mac-mini-de-caelum-7.local>
2016-09-22 12:20:06 -0300

    Add ContactMenuManager

---
+++ ContactMenuManager.h
@@ -0,0 +1,13 @@
+//
+//  ContactMenuManager.h
+//  contatosip67
+//
+//  Created by ios6246 on 9/22/16.
+//  Copyright © 2016 ios6246. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContactMenuManager : NSObject
+
+@end
